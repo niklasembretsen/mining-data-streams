046d80479a99264e8ab9f3831721c5fbf750203f
niklasembretsen <niklasembretsen@gmail.com>
2018-11-20 09:12:41 +0100

    added data set etc

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ name := "textual_similarity"
 version := "1.0"
 
 scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.2.1",
+  "org.apache.spark" %% "spark-sql" % "2.2.1",
+  "org.apache.spark" % "spark-streaming_2.11" % "2.2.1"
+)
--- miningDataStreams.scala
+++ miningDataStreams.scala
@@ -1,4 +1,14 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.storage.StorageLevel
 
 object MiningDataStreams {
-	
+
+	def main(args: Array[String]) {
+
+		// Create streaming context
+	    val conf = new SparkConf().setAppName("Lab 3").setMaster("local[*]")
+	    val ssc = new StreamingContext(conf, Seconds(1))
+	    
+	}
 }
