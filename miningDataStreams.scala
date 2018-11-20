d0a1b8a5e762ffd531984b45099e4ed991820387
niklasembretsen <niklasembretsen@gmail.com>
2018-11-20 12:52:54 +0100

    Keep implementin'

--- miningDataStreams.scala
+++ miningDataStreams.scala
@@ -29,39 +29,54 @@ object MiningDataStreams {
 			v = x.split(" ")(1)
 			sampleEdge(u, v, t) match{
 				case true => {
-					var currentUSet = reservoir.get(u).getOrElse(-1)
-					currentUSet match {
-						case -1 => {
-							reservoir += + (u -> Set(v))
-						}
-						case _ => {
-							reservoir += (u -> currentUSet + v)
-							updateCounters('+', u , v)
-						}
-					} 
-					var currentVSet = reservoir.get(v).getOrElse(-1)
-					currentVSet match{
-						case -1 => {
-							reservoir += (v -> Set(u))
-						}
-						case _ => {
-							reservoir += (v -> currentVSet + u)
-							updateCounters ('+', v, u)
-						}
-					}
+				// 	var currentUSet = reservoir.get(u).getOrElse(-1)
+				// 	currentUSet match {
+				// 		case -1 => {
+				// 			reservoir += + (u -> Set(v))
+				// 		}
+				// 		case _ => {
+				// 			reservoir += (u -> currentUSet + v)
+				// 			updateCounters('+', u , v)
+				// 		}
+				// 	} 
+				// 	var currentVSet = reservoir.get(v).getOrElse(-1)
+				// 	currentVSet match{
+				// 		case -1 => {
+				// 			reservoir += (v -> Set(u))
+				// 		}
+				// 		case _ => {
+				// 			reservoir += (v -> currentVSet + u)
+				// 			updateCounters ('+', v, u)
+				// 		}
+				// 	}
+					reservoir += (u -> (a.get(u).getOrElse(Set.empty) + v))
+					reservoir += (v -> (a.get(v).getOrElse(Set.empty) + u))
+					updateCounters('+', u, v)
 				}
 				case _ =>
 			}
-		}).filter(x => x != '%')
-
-
-		lines.map(x => (x.split(" ")(0), x.split(" ")(1))).print()
-
-		
+		})
 
 
 		ssc.start()
 		ssc.awaitTermination()
+	}
+
+	def sampleEdge(u: Int,
+					v: Int,
+					t: Int,
+					reservoir: Map[Int, Set[Int]]): Boolean = {
+
+		val m: Int = 1000
+	}
+
+	def updateCounters(operator: Char,
+						u: Int,
+						v: Int,
+						reservoir: Map[Int, Set[Int]],
+						edge2Triangles: Map[Int, Int],
+						triangleCount: Int) {
+
 	}
 
 }
