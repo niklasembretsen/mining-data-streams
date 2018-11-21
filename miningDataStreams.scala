9882e5fc9c4d5d23ab2b0fbab0031052d8f18c28
niklasembretsen <niklasembretsen@gmail.com>
2018-11-21 10:19:56 +0100

    Started with reservoir sampling

--- miningDataStreams.scala
+++ miningDataStreams.scala
@@ -4,9 +4,18 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.{
     StructType, StructField, StringType, IntegerType}
+import scala.util.Random
+import scala.collection.mutable.ListBuffer
 
 
 object MiningDataStreams {
+
+	// Reservoir
+	var reservoir: ListBuffer[(Int, Int)] = ListBuffer()
+	var adjacencyList: Map[Int, Set[Int]] = Map()
+	var edge2Triangles: Map[Int, Int] = Map()
+	var triangleCount: Int = 0;
+	var t: Int = 0;
 
 	def main(args: Array[String]) {		
 
@@ -14,11 +23,11 @@ object MiningDataStreams {
 	    val conf = new SparkConf().setAppName("Lab 3").setMaster("local[*]")
 	    val ssc = new StreamingContext(conf, Seconds(1))
 
-		// Reservoir	
-		var reservoir = Map[Int, Set[Int]] = Map()
-		var edge2Triangles = Map[Int, Int] = Map()
-		var triangleCount: Int = 0;
-		var t: Int = 0;
+		// adjacencyList
+		// var adjacencyList = Map[Int, Set[Int]] = Map()
+		// var edge2Triangles = Map[Int, Int] = Map()
+		// var triangleCount: Int = 0;
+		// var t: Int = 0;
 
 	    //read file from socket
 	    val lines = ssc.socketTextStream("localhost", 9999)
@@ -29,28 +38,29 @@ object MiningDataStreams {
 			v = x.split(" ")(1)
 			sampleEdge(u, v, t) match{
 				case true => {
-				// 	var currentUSet = reservoir.get(u).getOrElse(-1)
+				// 	var currentUSet = adjacencyList.get(u).getOrElse(-1)
 				// 	currentUSet match {
 				// 		case -1 => {
-				// 			reservoir += + (u -> Set(v))
+				// 			adjacencyList += + (u -> Set(v))
 				// 		}
 				// 		case _ => {
-				// 			reservoir += (u -> currentUSet + v)
+				// 			adjacencyList += (u -> currentUSet + v)
 				// 			updateCounters('+', u , v)
 				// 		}
 				// 	} 
-				// 	var currentVSet = reservoir.get(v).getOrElse(-1)
+				// 	var currentVSet = adjacencyList.get(v).getOrElse(-1)
 				// 	currentVSet match{
 				// 		case -1 => {
-				// 			reservoir += (v -> Set(u))
+				// 			adjacencyList += (v -> Set(u))
 				// 		}
 				// 		case _ => {
-				// 			reservoir += (v -> currentVSet + u)
+				// 			adjacencyList += (v -> currentVSet + u)
 				// 			updateCounters ('+', v, u)
 				// 		}
 				// 	}
-					reservoir += (u -> (a.get(u).getOrElse(Set.empty) + v))
-					reservoir += (v -> (a.get(v).getOrElse(Set.empty) + u))
+					adjacencyList += (u -> (a.get(u).getOrElse(Set.empty) + v))
+					adjacencyList += (v -> (a.get(v).getOrElse(Set.empty) + u))
+					reservoir :+= (u, v)
 					updateCounters('+', u, v)
 				}
 				case _ =>
@@ -62,20 +72,27 @@ object MiningDataStreams {
 		ssc.awaitTermination()
 	}
 
-	def sampleEdge(u: Int,
-					v: Int,
-					t: Int,
-					reservoir: Map[Int, Set[Int]]): Boolean = {
-
+	def sampleEdge(u: Int, v: Int,): Boolean = {
 		val m: Int = 1000
+
+		if (t <= m) {
+			//Inserted into the reservoir if t is less than M
+			return true
+		}
+		else if(Random.nextInt(t) >= m) {
+			//"coin flip" with p(H) = m/t and H => sample/replace
+			//Delete an edge uniformly at random
+			val edgeIdx: Int = Random.nextInt(reservoir.length)
+			val edge: (Int, Int) = reservoir(edgeIdx)
+
+			reservoir.remove(edgeIdx)
+			val u: Int = edge._1
+			val v: Int = edge._2
+
+		}
 	}
 
-	def updateCounters(operator: Char,
-						u: Int,
-						v: Int,
-						reservoir: Map[Int, Set[Int]],
-						edge2Triangles: Map[Int, Int],
-						triangleCount: Int) {
+	def updateCounters(operator: Char, u: Int, v: Int) {
 
 	}
 
